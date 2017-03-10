d5d25cb2556dd5b1269b39b32746f8243f40d693
Karan Kwatra <Karan Kwatra>
2017-03-10 14:46:49 -0500

    add sub, refactor

--- rpn.py
+++ rpn.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
+import operator
+
+OPERATORS = {
+    '+': operator.add,
+    '-': operator.sub,
+}
+
 def calculate(arg):
-    pass
+    stack = list()
+    for operand in arg.slit():
+        try:
+            operand = float(operand)
+            stack.append(operand)
+        except:
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            operator_fn = OPERATORS[operand]
+            result = operator_fn(arg1, arg2)
+
+            stack.append(result)
+
 def main():
     while True:
-        calculate(input("rpn calc> "))
-if __name__ == '__main__': # Note: that's "underscore underscore n a m e ..."
+        result = calculate(input('rpn calc> '))
+
+if __name__ == '__main__':
     main()
