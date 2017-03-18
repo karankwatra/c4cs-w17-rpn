476c5f15a76d61e3792444809365d05f62b3d907
Karan Kwatra <Karan Kwatra>
2017-03-18 12:39:16 -0400

    added travis file

--- rpn.py
+++ rpn.py
@@ -1,30 +1,28 @@
 #!/usr/bin/env python3
 import operator
-
 OPERATORS = {
-    '+': operator.add,
-    '-': operator.sub,
+	'+': operator.add,
+	'-': operator.sub,
+	'*': operator.mul,
+	'/': operator.truediv,
 }
-
 def calculate(arg):
-    stack = list()
-    for operand in arg.slit():
-        try:
-            operand = float(operand)
-            stack.append(operand)
-        except:
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            operator_fn = OPERATORS[operand]
-            result = operator_fn(arg1, arg2)
+	stack = list()
+	for operand in arg.split():
+		try:
+			operand = float(operand)
+			stack.append(operand)
+		except:
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			operator_fn = OPERATORS[operand]
+			result = operator_fn(arg1, arg2)
 
-            stack.append(result)
-    return stack.pop()
-
+			stack.append(result)
+	return stack.pop()
 def main():
-    while True:
-        result = calculate(input('rpn calc> '))
-        print("Result:", result)
-
+	while True:
+		result = calculate(input('rpn calc> '))
+		print("Result:", result)
 if __name__ == '__main__':
-    main()
+	main()
