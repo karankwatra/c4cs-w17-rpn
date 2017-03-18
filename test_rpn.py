6be9ea2df655ad7a597d0c8e1f5b51f5db0d6d20
Karan Kwatra <Karan Kwatra>
2017-03-18 13:00:37 -0400

    attempt to fix weird indentation error

--- test_rpn.py
+++ test_rpn.py
@@ -1,5 +1,6 @@
 import unittest
 import rpn
+
 class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate('1 1 +')
@@ -7,6 +8,6 @@ class TestBasics(unittest.TestCase):
 	def test_subtract(self):
 		result = rpn.calculate('5 3 -')
 		self.assertEqual(2, result)
-    def test_exponent(self):
-        result = rpn.calculate('2 3 ^')
-        self.assertEqual(8, result)
+	def test_exponent(self):
+		result = rpn.calculate('2 3 ^')
+		self.assertEqual(8, result)
