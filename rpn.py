300ef1fee255a304535fde6055bab1a1ac05a19e
Karan Kwatra <Karan Kwatra>
2017-03-18 12:51:33 -0400

    retrying build

--- rpn.py
+++ rpn.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
+
 import operator
+
 OPERATORS = {
 	'+': operator.add,
 	'-': operator.sub,
 	'*': operator.mul,
 	'/': operator.truediv,
 }
+
 def calculate(arg):
 	stack = list()
 	for operand in arg.split():
@@ -20,9 +23,11 @@ def calculate(arg):
 
 			stack.append(result)
 	return stack.pop()
+
 def main():
 	while True:
 		result = calculate(input('rpn calc> '))
 		print("Result:", result)
+
 if __name__ == '__main__':
 	main()
